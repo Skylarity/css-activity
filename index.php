287f5982d8870b5d81f364170b83e3f0aea8e235
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-07-20 15:00:01 -0600

    add content

--- index.php
+++ index.php
@@ -11,8 +11,75 @@
 			<p><small>Cats + Development + CSS = <3</small></p>
 		</header>
 		<main>
-			<img src="images/moar.jpg" alt="more cat"/>
+			<img src="images/moar-200.jpg" alt="more cat"/>
 			<p>I'd like to think that I haven't changed those things, sir. Maybe we better talk out here; the observation lounge has turned into a swamp. Commander William Riker of the Starship Enterprise. This should be interesting. You bet I'm agitated! I may be surrounded by insanity, but I am not insane. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Mr. Worf, you sound like a man who's asking his friend if he can start dating his sister. You did exactly what you had to do. You considered all your options, you tried every alternative and then you made the hard choice. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. That might've been one of the shortest assignments in the history of Starfleet. Well, that's certainly good to know. I've had twelve years to think about it. And if I had it to do over again, I would have grabbed the phaser and pointed it at you instead of them.</p>
+			<h2>Table of Commenting Syntax</h2>
+			<table>
+				<tr>
+					<th>Language</th>
+					<th>Syntax</th>
+				</tr>
+				<tbody>
+					<tr>
+						<td>C++, Java, PHP, CSS, JavaScript</td>
+						<td>//This is a comment.
+						</td>
+					</tr>
+					<tr>
+						<td>MySQL</td>
+						<td>-- This is a comment.</td>
+					</tr>
+					<tr>
+						<td>HTML</td>
+						<td>&lt;!--This is a comment.--&gt;</td>
+					</tr>
+					<tr>
+						<td>MatLab</td>
+						<td>%This is a comment.</td>
+					</tr>
+					<tr>
+						<td>Ruby, Python</td>
+						<td>#This is a comment.</td>
+					</tr>
+				</tbody>
+			</table>
+			<h3>This is a form.</h3>
+			<form method="post" action="#" id="sample-form">
+				<div class="form-group">
+					<label for="txtName">Your Name:</label>
+					<input type="text" id="txtName" placeholder="Your Name Here." maxlength="50"/>
+				</div>
+				<div class="form-group">
+					<label for="emailEmail">Your Email:</label>
+					<input id="emailEmail" type="email" placeholder="Your Email Here." maxlength="100"/>
+				</div>
+				<div class="form-group">
+					<label for="selectLang">Choose Your Language:</label><br/>
+					<select id="selectLang" class="form-control">
+						<option class="no-option" value="" disabled selected>Choose One! :D</option>
+						<option value="PHP">PHP</option>
+						<option value="JavaScript">JavaScript</option>
+						<option value="CSS">Cobol</option>
+						<option value="LOLCODE">LOLCODE</option>
+						<option value="Klingon">Klingon (Qapla'!)</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<div>
+						<label for="txtareaComments">Tell Us More About Yourself:</label>
+					</div>
+					<textarea rows="5" id="txtareaComments" maxlength="500" placeholder="500 Characters Max."></textarea>
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn">Submit</button>
+				</div>
+			</form>
+			<img src="http://placekitten.com/g/200/300" alt="placekitten"/>
+			<img src="http://placekitten.com/g/200/300" alt="placekitten"/>
+			<img src="http://placekitten.com/g/200/300" alt="placekitten"/>
+			<p>But the probability of making a six is no greater than that of rolling a seven. I recommend you don't fire until you're within 40,000 kilometers. Well, I'll say this for him - he's sure of himself. Ensign Babyface! I'll be sure to note that in my log. Sure. You'd be surprised how far a hug goes with Geordi, or Worf.</p>
+			<p>You enjoyed that. A lot of things can change in twelve years, Admiral. I'll alert the crew. What? We're not at all alike! Your head is not an artifact! Why don't we just give everybody a promotion and call it a night - 'Commander'? and attack the Romulans. I suggest you drop it, Mr. Data. Congratulations - you just destroyed the Enterprise.</p>
+			<p>And blowing into maximum warp speed, you appeared for an instant to be in two places at once. Mr. Worf, you do remember how to fire phasers? How long can two people talk about nothing? Some days you get the bear, and some days the bear gets you. Mr. Crusher, ready a collision course with the Borg ship. Fate protects fools, little children and ships named Enterprise. We finished our first sensor sweep of the neutral zone.</p>
 		</main>
 	</body>
 </html>
