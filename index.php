8bc012cb5bbf5a785521d966e81fa6140183ea4f
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-07-20 14:05:49 -0600

    add index, image, content

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="css/stylesheet.css" type="text/css"/>
+		<title>CSS Activity</title>
+	</head>
+	<body>
+		<header>
+			<h1>Best Webpage Evar.</h1>
+			<p><small>Cats + Development + CSS = <3</small></p>
+		</header>
+		<main>
+			<img src="images/moar.jpg" alt="more cat"/>
+			<p>I'd like to think that I haven't changed those things, sir. Maybe we better talk out here; the observation lounge has turned into a swamp. Commander William Riker of the Starship Enterprise. This should be interesting. You bet I'm agitated! I may be surrounded by insanity, but I am not insane. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Mr. Worf, you sound like a man who's asking his friend if he can start dating his sister. You did exactly what you had to do. You considered all your options, you tried every alternative and then you made the hard choice. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. That might've been one of the shortest assignments in the history of Starfleet. Well, that's certainly good to know. I've had twelve years to think about it. And if I had it to do over again, I would have grabbed the phaser and pointed it at you instead of them.</p>
+		</main>
+	</body>
+</html>
